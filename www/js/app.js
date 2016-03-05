884aa6086b9f6e9b1d8eae92347098043dca17c2
Esteban Samuel Reyes <estebansamuel.reyes@gmail.com>
2016-03-05 15:18:59 -0430

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,103 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/settings.html',
+    controller: 'AppCtrl'
+  })
+
+
+    .state('app.menu', {
+      url: '/menu',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/menu.html'
+        }
+      }
+    })
+    .state('app.info', {
+      url: '/info',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/info.html'
+          //controler
+        }
+      }
+    })
+        .state('app.color', {
+      url: '/color',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/color.html'
+          //controler
+        }
+      }
+    })
+            .state('app.smell', {
+      url: '/smell',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/smell.html'
+          //controler
+        }
+      }
+    })
+
+    .state('app.taste', {
+      url: '/taste',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/taste.html'
+          //controler
+        }
+      }
+    })
+     .state('app.resumen', {
+      url: '/resumen',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/resumen.html'
+          //controler
+        }
+      }
+    })
+     .state('app.mynotes', {
+      url: '/Notes',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/Mynotes.html'
+          //controler
+        }
+      }
+    })
+
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/menu');
+});
